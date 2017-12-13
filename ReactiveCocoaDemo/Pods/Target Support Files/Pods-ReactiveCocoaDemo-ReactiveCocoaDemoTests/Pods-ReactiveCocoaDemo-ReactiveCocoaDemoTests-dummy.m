b8df0437e7169b32acc39d35f1e3c068b4b22fb3
Bean He <withhebing@gmail.com>
2017-12-13 11:35:53 +0800

    pod install

---
+++ ReactiveCocoaDemo/Pods/Target Support Files/Pods-ReactiveCocoaDemo-ReactiveCocoaDemoTests/Pods-ReactiveCocoaDemo-ReactiveCocoaDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactiveCocoaDemo_ReactiveCocoaDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactiveCocoaDemo_ReactiveCocoaDemoTests
+@end
---
+++ ReactiveCocoaDemo/Pods/Target Support Files/Pods-ReactiveCocoaDemo-ReactiveCocoaDemoUITests/Pods-ReactiveCocoaDemo-ReactiveCocoaDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactiveCocoaDemo_ReactiveCocoaDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactiveCocoaDemo_ReactiveCocoaDemoUITests
+@end
